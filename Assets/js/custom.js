86da3e52c91be9e96a933857fbbf314c8725252d
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2021-10-31 18:27:08 +0100

    updates on alumni

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -597,3 +597,23 @@ function mapRange(value, fromIn, toIn, fromOut, toOut) {
 //end of gallery
 
 
+//for alumni
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+	if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : 
+        decodeURIComponent(sParameterName[1]);
+   }
+  }
+};
+if(getUrlParameter('register') == 'true')
+{
+   document.getElementById('flipcard-inner').style.transform = 'rotateY(180deg)';			
+		}
