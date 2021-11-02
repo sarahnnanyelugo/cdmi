d7738663295e834cf761551369d28c9370e59542
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2021-11-02 07:42:13 +0100

    updates on FAQs

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -617,3 +617,147 @@ if(getUrlParameter('register') == 'true')
 {
    document.getElementById('flipcard-inner').style.transform = 'rotateY(180deg)';			
 		}
+
+
+
+        //for FAQ
+
+       function myFunction() {
+          var dots = document.getElementById("dots");
+          var moreText = document.getElementById("more");
+          var btnText = document.getElementById("myBtn");
+
+          if (dots.style.display === "none") {
+            dots.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+          }
+        }
+
+
+        function myFunction2() {
+          var dots2 = document.getElementById("dots2");
+          var moreText = document.getElementById("more2");
+          var btnText = document.getElementById("myBtn2");
+
+          if (dots2.style.display === "none") {
+            dots2.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots2.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+          }
+        }
+
+
+        function myFunction3() {
+          var dots3 = document.getElementById("dots3");
+          var moreText = document.getElementById("more3");
+          var btnText = document.getElementById("myBtn3");
+
+          if (dots3.style.display === "none") {
+            dots3.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots3.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+          }
+        }
+
+        function myFunction4() {
+          var dots4 = document.getElementById("dots4");
+          var moreText = document.getElementById("more4");
+          var btnText = document.getElementById("myBtn4");
+
+          if (dots4.style.display === "none") {
+            dots4.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots4.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+          }
+        }
+
+        function myFunction5() {
+          var dots5 = document.getElementById("dots5");
+          var moreText = document.getElementById("more5");
+          var btnText = document.getElementById("myBtn5");
+
+          if (dots5.style.display === "none") {
+            dots5.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots5.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+          }
+        }
+        function myFunction6() {
+          var dots6 = document.getElementById("dots6");
+          var moreText = document.getElementById("more6");
+          var btnText = document.getElementById("myBtn6");
+
+          if (dots6.style.display === "none") {
+            dots6.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots6.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+
+          }
+        }
+
+        function myFunction7() {
+          var dots7 = document.getElementById("dots7");
+          var moreText = document.getElementById("more7");
+          var btnText = document.getElementById("myBtn7");
+
+          if (dots7.style.display === "none") {
+            dots7.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots7.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+
+          }
+        }
+
+        function myFunction8() {
+          var dots8 = document.getElementById("dots8");
+          var moreText = document.getElementById("more8");
+          var btnText = document.getElementById("myBtn8");
+
+          if (dots8.style.display === "none") {
+            dots8.style.display = "block";
+            btnText.innerHTML = "";
+            moreText.style.display = "none";
+          } else {
+            dots8.style.display = "none";
+            btnText.innerHTML = "";
+            moreText.style.display = "block";
+
+
+          }
+        }
