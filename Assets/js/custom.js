dc28ddb1f4688783b86f7fdd3a0d47830cf51223
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2021-11-04 01:03:52 +0100

    updates on welcome address

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -84,12 +84,16 @@ $(function () {
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  $("#vic").removeClass("viclarge");
+  $("#vic").addClass("vicsmall");
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  $("#vic").removeClass("vicsmall");
+  $("#vic").addClass("viclarge")
 }
 
 
@@ -761,3 +765,6 @@ if(getUrlParameter('register') == 'true')
 
           }
         }
+        //for welcome address
+
+
